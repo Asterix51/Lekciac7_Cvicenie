e8e7fc727683629c826c4034fe8d3cc37a867139
Asterix5 <asterix5@azet.sk>
2024-12-07 08:03:27 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekciac7_Cvicenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
